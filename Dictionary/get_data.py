559bbfe637099428ab12bc54d952a89d0c7d7c7f
Michael Littleton <michaelglittleton@gmail.com>
2020-12-28 22:01:29 -0500

    Initial program start
     - Imported data into program
    
     - Allowed user input for word to search for

---
+++ Dictionary/get_data.py
@@ -0,0 +1,22 @@
+import json
+
+data = json.load(open('data.json'))
+word = ''
+
+
+def get_word(st):
+    if st in data:
+        return data[st]
+    else:
+        return 0
+
+
+print('What word would you like to search for? ')
+
+while word == '':
+    word = input()
+    break
+
+# TODO: uppercase input
+# TODO: loop for endless input
+# TODO: loop for multiple searches
