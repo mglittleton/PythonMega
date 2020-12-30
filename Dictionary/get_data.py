129a46c0a2d4c80456ec5ecb6ad7114f933602c8
Michael Littleton <michaelglittleton@gmail.com>
2020-12-29 21:26:09 -0500

    Added loops and close match

--- Dictionary/get_data.py
+++ Dictionary/get_data.py
@@ -1,7 +1,10 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 word = ''
+words = list(data)
+running = True
 
 
 def get_word(st):
@@ -11,12 +14,39 @@ def get_word(st):
         return 0
 
 
-print('What word would you like to search for? ')
+while running:
 
-while word == '':
-    word = input()
-    break
+    print('----------------------------------------')
+    print('What word would you like to search for? ')
 
-# TODO: uppercase input
-# TODO: loop for endless input
-# TODO: loop for multiple searches
+    while word == '':
+        word = input().lower()
+        if word not in data:
+            close = get_close_matches(word, words)
+            if not close:
+                close = ["<error>"]
+            print("That word does not exist.")
+            print("Did you mean {}.".format(close[0]))
+            affirm = input("(Y)es or (N)o - ")
+            if affirm == 'n' or affirm == "N":
+                print('----------------------------------------')
+                print('Please try entering the word again.')
+                word = ''
+                continue
+            word = close[0]
+
+        counter = 0
+        print('----------------------------------------')
+        print("{} - ".format(word))
+        for definition in data[word]:
+            counter += 1
+            print("{0}. {1}".format(counter, definition))
+
+    print("Would you like to search for something else?")
+    loop = input("(Y)es or (N)o - ")
+    if loop == 'n' or loop == 'N':
+        running = False
+    else:
+        word = ''
+
+
